c70e88b938d96e58a6640c7fbf132fd6dfee3c49
Zekereis <zekereisoglu@outlook.com>
2023-01-31 18:15:39 +0300

    done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,51 @@
+const Accounts = require("./accounts-model")
+const db = require("../../data/db-config")
+
 exports.checkAccountPayload = (req, res, next) => {
   // KODLAR BURAYA
   // Not: Validasyon için Yup(şu an yüklü değil!) kullanabilirsiniz veya kendiniz manuel yazabilirsiniz.
+  const error = { status: 400}
+  const {name , budget} = req.body
+  if(name === undefined || budget === undefined){
+    error.message = "ad ve bütçe gereklidir"
+  }else if(name.trim().length < 3 || name.trim().length > 100){
+    error.message = "Hesabın adı 3 ile 100 arasında olmalı"
+  }else if(typeof budget !== "number" || isNaN(budget)){
+    error.message = "hesabın bütçesi bir sayı olmalıdır"
+  }else if(budget < 0 || budget > 1000000){
+    error.message = "hesap bütcesi çok büyük veya çok küçük"
+  }
+
+  if(error.message){
+    next(error)
+  }else{
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // KODLAR BURAYA
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existing = await db("accounts").where("name",req.body.name.trim()).first()
+    if(existing){
+      next({status: 400, message: "bu ad alınmış"})
+    }else{
+      next()
+    }
+  }catch (err){
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // KODLAR BURAYA
+exports.checkAccountId =async (req, res, next) => {
+  try{
+    const account = await Accounts.getById(req.params.id)
+    if(!account){
+      next({status:404, message: "hesap bulunamadı"})
+    }else{
+      req.account = account
+      next()
+    }
+  }catch(err){
+    next(err)
+  }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,60 @@
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
-  // KODLAR BURAYA
+const md = require("./accounts-middleware")
+const Account = require("./accounts-model")
+
+router.get('/', async (req, res, next) => {
+  try{
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  }catch(err){
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+router.get('/:id', md.checkAccountId , async (req, res, next) => {
+ try{
+  res.json(req.account)
+ }catch(err){
+  next(err)
+ }
 })
 
-router.post('/', (req, res, next) => {
-  // KODLAR BURAYA
+router.post('/', md.checkAccountPayload,md.checkAccountNameUnique,async (req, res, next) => {
+  try{
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget
+    })
+    res.status(201).json(newAccount)
+  }catch(err) {
+    next(err)
+  }
+  
 })
 
-router.put('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+router.put('/:id',md.checkAccountId,md.checkAccountPayload,async(req, res, next) => {
+  try{
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
+  }catch(err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+router.delete('/:id',md.checkAccountId, async (req, res, next) => {
+  try{
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
+  }catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // KODLAR BURAYA
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
